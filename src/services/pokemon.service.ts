ef56f1ccdf2b02d806642b1bb421cca6b7492736
mlucero <mauricio.lucero2014@gmail.com>
2024-09-19 23:24:48 -0300

    Add static pagination and pokemon detail page

--- src/services/pokemon.service.ts
+++ src/services/pokemon.service.ts
@@ -1,7 +1,7 @@
 import type { PokemonListResponse } from '../types/pokemon.types';
 
 export async function getPokemons() {
-  const res = await fetch('https://pokeapi.co/api/v2/pokemon');
+  const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
 
   return (await res.json()) as PokemonListResponse;
 }
