7467cbb91ccdf4dc96085b10d61e96fd2a4d1e21
mlucero <mauricio.lucero2014@gmail.com>
2024-10-29 13:08:28 -0300

    Remove view transitions for performance reasons and change bun with pnpm

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
-import solidJs from "@astrojs/solid-js";
+import solidJs from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()]
+  redirects: {
+    '/': '/pokemons/1',
+  },
+  integrations: [tailwind(), solidJs()],
 });
